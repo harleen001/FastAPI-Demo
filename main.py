360da2c7e2fea7e98f0bc1925e66b67feaa3507e
harleen001 <harleen.johal31@gmail.com>
2026-01-31 16:49:07 +0530

    app-get

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from fastapi import FastAPI
 
 app=FastAPI()
 
-
+@app.get("/")
 def greet():
     return "Welcome to fast api project"
+
+@app.get("/products")
+def get_all_products():
+    return "all products"
